9e39d63995d09489fb0b9649fdf5abf9bae0a666
Higor Barbosa <higorpereirabarbosa@gmail.com>
2019-08-26 14:06:52 -0300

    Criando model e view Departaments

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -5,27 +5,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SalesWebMVC.Models;
+using SalesWebMVC.Models.ViewModels;
 
 namespace SalesWebMVC.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : Controller //todo controlador herda de Controler. O nome Home no inicil da classe determina a pasta da View correspondente
     {
-        public IActionResult Index()
+        public IActionResult Index() // IActionResult tipo generico de um resultado 
         {
             return View();
         }
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
-
+            ViewData["Message"] = "Aplicitavo de vendas do curso de C#";
+            ViewData["email"] = "higorpereirabarbosa@gmail.com";
             return View();
         }
 
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
-
+            ViewData["email"] = "higorpereirabarbosa@gmail.com";
             return View();
         }
 
